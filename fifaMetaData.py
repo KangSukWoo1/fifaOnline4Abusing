64a5517d18c50357ae1a44b3f1ca4b2138b559dd
goldenbooma <goldenbooma@op.gg>
2023-05-25 15:45:47 +0000

    시즌 변경으로 인한 기존 닉네임 extend 및 type error 방어로직설계

--- fifaMetaData.py
+++ fifaMetaData.py
@@ -1,8 +1,10 @@
 import requests
-import json
-with open('config.json','r', encoding = 'utf-8') as file:
-    config = json.load(file)
-api_key = config['data']['API_KEY']
+import os
+import sys
+from dotenv import load_dotenv
+sys.path.append("/home/ubuntu/Study/fifaOnline4Abusing")
+load_dotenv(verbose=True)
+api_key = os.environ['API_KEY']
 HEADERS = {"Authorization" : api_key}
 seasonId = requests.get('https://static.api.nexon.co.kr/fifaonline4/latest/seasonid.json', headers = HEADERS).json()
 spid = requests.get("https://static.api.nexon.co.kr/fifaonline4/latest/spid.json", headers = HEADERS).json()
--- nickNameCrawling.py
+++ nickNameCrawling.py
@@ -55,25 +55,30 @@ async def get_api_data():
 
         nicknames = []
         tasks = [get_nickname(session, rankerid) for rankerid in ranker_ids]
+        nickname_values = pd.read_json('rankerInfo.json')['nickname'].values
+        coroutines = [get_nickname(session, nickname) for nickname in nickname_values]
+        tasks.extend([asyncio.ensure_future(coroutine) for coroutine in coroutines])
         results = await asyncio.gather(*tasks)
         nicknames.extend(results)
 
         access_ids = [nickname.get('accessId') for nickname in nicknames]
-
+        
         buy_data = []
         tasks = [get_market_data(session, access_id) for access_id in access_ids]
         results = await asyncio.gather(*tasks)
         buy_data.extend(results)
     buy_data_decomposition = []
     for one_data in buy_data:
-        if one_data['accessId'] == None : 
-            pass
-        else : 
+        try : 
+            if one_data['accessId'] is None : 
+                continue
             access_id = one_data['accessId']
             for b in one_data['buy']:
                 buy_data_decomposition.append({'accessId': access_id, **b, 'type': 'buy'})
             for s in one_data['sell']:
                 buy_data_decomposition.append({'accessId': access_id, **s, 'type': 'sell'})
+        except TypeError:
+            continue
     api_data = pd.DataFrame(buy_data_decomposition)
 
     return api_data
